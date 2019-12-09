fce894f360b623b83263d45e4690fab1b5705f49
jtobin321 <jtobin321@gmail.com>
2019-12-09 11:02:32 -0500

    updated grid and column

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Container, Menu, Icon, Button } from 'semantic-ui-react'
+import { Grid, Container, Menu, Icon, Input } from 'semantic-ui-react'
 
 import UserProfileCard from './components/UserProfileCard'
 import './styles/app.css'
@@ -22,16 +22,21 @@ export default class extends Component {
     return (
       <div>
         <Menu attached='top' borderless inverted>
-        <Menu.Item>
-          <img src='https://api.adorable.io/avatars/1' />
-        </Menu.Item>
-        <Menu.Item header>Users Wiki</Menu.Item>
-        <Menu.Item position="right" href="https://github.com/jtobin321/Users-Wiki" target="_blank">
-          <Icon name='github' size='big' link hover />
-        </Menu.Item>
+          <Menu.Item>
+            <img src='https://api.adorable.io/avatars/1' />
+          </Menu.Item>
+          <Menu.Item header>Users Wiki</Menu.Item>
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Input className='icon' icon='search' placeholder='Search Users...' />
+            </Menu.Item>
+            <Menu.Item position="right" href="https://github.com/jtobin321/Users-Wiki" target="_blank">
+              <Icon name='github' size='big' link hover />
+            </Menu.Item>
+          </Menu.Menu>
         </Menu>
         <Container>
-        <Grid columns={4} padded>
+        <Grid columns={4} padded stackable>
               {this.state.users.map(user => {
                 return (
                   <Grid.Column>
